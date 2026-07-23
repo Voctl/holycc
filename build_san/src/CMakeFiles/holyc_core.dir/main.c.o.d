57e0752c049b3da1020eb79ce00d78af9a058f72
Voctl <denis.3131a@gmail.com>
2026-07-23 14:37:28 +0400

    chore: ignore build_san directory

--- build_san/src/CMakeFiles/holyc_core.dir/main.c.o.d
+++
@@ -1,4 +0,0 @@
-src/CMakeFiles/holyc_core.dir/main.c.o: \
- /home/devuan/projects/holyc/src/main.c /usr/include/stdc-predef.h \
- /home/devuan/projects/holyc/include/holyc/driver.h \
- /usr/lib/gcc/x86_64-linux-gnu/12/include/stdbool.h
