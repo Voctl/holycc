25b5c2fa6aca858017e3a77a066b5d4f953b2be6
Voctl <denis.3131a@gmail.com>
2026-07-27 17:52:39 +0400

    docs: add comments to codegen.h

--- include/holyc/codegen.h
+++ include/holyc/codegen.h
@@ -5,11 +5,14 @@
 #include "holyc/symbol.h"
 #include <stdio.h>
 
+// Opaque code-generator state
 typedef struct CodeGen CodeGen;
 
+// Create/destroy the C17 code generator
 CodeGen *codegen_create(SymbolTable *symtab);
 void codegen_destroy(CodeGen *cg);
 
+// Emit C17 source text – to an open FILE or directly to a file path
 bool codegen_generate(CodeGen *cg, AstNode *ast, FILE *output);
 bool codegen_generate_file(CodeGen *cg, AstNode *ast, const char *output_path);
 
