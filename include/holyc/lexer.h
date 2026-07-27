b7b862d59445189c6b0423c820c01b6fc5508356
Voctl <denis.3131a@gmail.com>
2026-07-27 17:52:39 +0400

    docs: add comments to lexer.h

--- include/holyc/lexer.h
+++ include/holyc/lexer.h
@@ -5,12 +5,18 @@
 #include "holyc/diag.h"
 #include <stddef.h>
 
+// Opaque lexer state
 typedef struct Lexer Lexer;
 
+// Create/destroy a lexer over a source buffer
 Lexer *lexer_create(const char *filename, const char *source, size_t source_len);
 void lexer_destroy(Lexer *lexer);
+
+// Token stream – pull next token or peek without consuming
 Token lexer_next_token(Lexer *lexer);
 Token lexer_peek_token(Lexer *lexer);
+
+// Attach a diagnostics collector for error reporting
 void lexer_set_diagnostics(Lexer *lexer, Diagnostics *diag);
 
 #endif
