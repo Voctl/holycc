0dcac0ff66b54d038b70b7acffd4b7910993d200
Voctl <denis.3131a@gmail.com>
2026-07-27 17:52:39 +0400

    docs: add comments to parser.h

--- include/holyc/parser.h
+++ include/holyc/parser.h
@@ -5,12 +5,15 @@
 #include "holyc/lexer.h"
 #include "holyc/diag.h"
 
+// Opaque parser state
 typedef struct Parser Parser;
 
+// Create/destroy a recursive-descent parser
 Parser *parser_create(Lexer *lexer, Diagnostics *diag);
 void parser_set_sourcedir(Parser *parser, const char *dir);
 void parser_destroy(Parser *parser);
 
+// Parse a full translation unit (the entire source file)
 AstNode *parser_parse_translation_unit(Parser *parser);
 
 #endif
